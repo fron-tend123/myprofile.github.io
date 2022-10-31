a9c3e4270b843f9f45d42fff6a487f29eaba0c05
fron-tend123 <95183241+fron-tend123@users.noreply.github.com>
2022-10-31 18:45:20 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function link1(){
+  document.getElementByid('man').href= 'https://www.facebook.com/lukagabelaia777'
+}
+
+
+function link2(){
+  document.getElementByid('man').href= 'https://www.instagram.com/lukagabe777/'
+}
+
+
+
+function link3(){
+  document.getElementByid('man').href= 'https://www.linkedin.com/in/front-end-937b55252'
+  
+}
+
+
+function link4(){
+  document.getElementByid('man').href= 'https://twitter.com/_lukas_edits?t=yu3H1AvRYrplVr3-FUmRVA&s=09'
+}
